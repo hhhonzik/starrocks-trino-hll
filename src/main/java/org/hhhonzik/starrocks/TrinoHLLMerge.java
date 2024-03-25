0df76f118c853785a4a65de6af1f0c269ce653b5
Honza Stepanovsky <jstepanovsky@id5.io>
2024-03-26 00:48:43 +0100

    add trino hll merge

---
+++ src/main/java/org/hhhonzik/starrocks/TrinoHLLMerge.java
@@ -0,0 +1,59 @@
+package org.hhhonzik.starrocks;
+
+
+import com.facebook.airlift.stats.cardinality.HyperLogLog;
+import java.util.Base64;
+import static io.airlift.slice.Slices.wrappedBuffer;
+
+
+import io.airlift.slice.Slice;
+
+public class TrinoHLLMerge {
+    public static class State {
+        HyperLogLog hll = HyperLogLog.newInstance(standardErrorToBuckets(DEFAULT_STANDARD_ERROR));
+        public int serializeLength() { return 4; }
+    }
+
+    public State create() {
+        return new State();
+    }
+
+    public void destroy(State state) {
+    }
+
+    public final void update(State state, String serializedHll) {
+        if (serializedHll != null) {
+            byte[] bytes = Base64.getDecoder().decode(serializedHll);
+            Slice s = wrappedBuffer(bytes);
+            HyperLogLog other = HyperLogLog.newInstance(s);
+    
+            state.hll.mergeWith(other);
+        }
+    }
+
+    public void serialize(State state, java.nio.ByteBuffer buff) {
+        buff.put(state.hll.serialize().getBytes());
+    }
+
+    public void merge(State state, java.nio.ByteBuffer buffer) {
+        Slice s = wrappedBuffer(buffer);
+        HyperLogLog other = HyperLogLog.newInstance(s);
+
+        state.hll.mergeWith(other);
+    }
+
+    public Long finalize(State state) {
+        return state.hll.cardinality();
+    }
+
+    // taken from prestodb/presto
+    public static final double DEFAULT_STANDARD_ERROR = 0.01625;
+
+    public static int standardErrorToBuckets(double maxStandardError) {
+        return log2Ceiling((int) Math.ceil(1.0816 / (maxStandardError * maxStandardError)));
+    }
+
+    private static int log2Ceiling(int value) {
+        return Integer.highestOneBit(value - 1) << 1;
+    }
+}
