291427b0c85aa540c30ba23b5d488e3485244e20
Honza Stepanovsky <jstepanovsky@id5.io>
2024-03-25 22:56:19 +0100

    dot

--- src/main/java/org/hhhonzik/starrocks/TrinoHLLCardinality.java
+++ src/main/java/org/hhhonzik/starrocks/TrinoHLLCardinality.java
@@ -1,11 +1,23 @@
 package org.hhhonzik.starrocks;
 // import com.facebook.airlift.stats.cardinality.HyperLogLog;
 
+import com.facebook.airlift.stats.cardinality.HyperLogLog;
+
 public class TrinoHLLCardinality {
-        public final Long evaluate(String obj) {
-        System.err.println(obj);
+
+    // taken from prestodb/presto
+    public static final double DEFAULT_STANDARD_ERROR = 0.01625;
+
+    public static int standardErrorToBuckets(double maxStandardError) {
+        return log2Ceiling((int) Math.ceil(1.0816 / (maxStandardError * maxStandardError)));
+    }
+
+    public final Long evaluate(String serializedHll) {
+        // return HyperLogLog.newInstance(serializedHll).cardinality();
         return 123L;
     }
+
+    private static int log2Ceiling(int value) {
+        return Integer.highestOneBit(value - 1) << 1;
+    }
 }
-
-
