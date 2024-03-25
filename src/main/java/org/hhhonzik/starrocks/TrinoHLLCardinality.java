e14e0459b0e6d1c51201c01bfefc16d750410c9d
Honza Stepanovsky <jstepanovsky@id5.io>
2024-03-25 22:43:35 +0100

    type mapping to string

--- src/main/java/org/hhhonzik/starrocks/TrinoHLLCardinality.java
+++ src/main/java/org/hhhonzik/starrocks/TrinoHLLCardinality.java
@@ -1,10 +1,9 @@
 package org.hhhonzik.starrocks;
-import java.nio.Buffer;
 // import com.facebook.airlift.stats.cardinality.HyperLogLog;
 
 public class TrinoHLLCardinality {
-
-        public final Long evaluate(Buffer obj) {
+        public final Long evaluate(String obj) {
+        System.err.println(obj);
         return 123L;
     }
 }
